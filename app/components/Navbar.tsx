e3187a7e9670a02b57269f9e8aab63c52ce9f340
Nick Carpenter <npcarp1@gmail.com>
2025-12-23 15:43:08 -0500

    Make navbar align with page content

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,24 +2,24 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="mx-auto max-w-3xl px-6 py-4">
-      <div className="flex items-center justify-between">
-        {/* Left: Name */}
-        <Link href="/" className="font-semibold tracking-tight">
-          Nick Carpenter
-        </Link>
+    <nav className="w-full">
+      <div className="mx-auto max-w-3xl px-6 py-4">
+        <div className="flex items-center justify-between">
+          <Link href="/" className="font-semibold tracking-tight hover:opacity-80">
+            Nick Carpenter
+          </Link>
 
-        {/* Right: Nav */}
-        <div className="flex items-center gap-6 text-sm text-gray-300">
-          <Link href="/projects" className="hover:text-white underline-offset-4 hover:underline">
-            Projects
-          </Link>
-          <Link href="/about" className="hover:text-white underline-offset-4 hover:underline">
-            About
-          </Link>
-          <Link href="/contact" className="hover:text-white underline-offset-4 hover:underline">
-            Contact
-          </Link>
+          <div className="flex items-center gap-6 text-sm">
+            <Link href="/projects" className="underline underline-offset-4">
+              Projects
+            </Link>
+            <Link href="/about" className="underline underline-offset-4">
+              About
+            </Link>
+            <Link href="/contact" className="underline underline-offset-4">
+              Contact
+            </Link>
+          </div>
         </div>
       </div>
     </nav>
